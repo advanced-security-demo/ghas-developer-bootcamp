0f85df4d65c4f2e648a3573ae6ed836f04f80a07
Stefan Petrushevski <theztefan@github.com>
2023-11-24 09:44:40 +0100

    remove unused files

--- ghas-api/get_alerts.py
+++
@@ -1,8 +0,0 @@
-from ghapi.all import GhApi
-from datetime import datetime, timedelta
-from pprint import pprint
-
-api = GhApi(owner='advanced-security-demo')
-
-for alert in api.code_scanning.list_alerts_for_repo(repo="bertelsmann-demo", state="open"):
-    pprint(alert)   
